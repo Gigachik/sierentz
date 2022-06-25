b58a75f23db5d33f3ef5ab0942aba6bb34fac8fa
Gigachik <56297260+Gigachik@users.noreply.github.com>
2022-06-25 20:24:53 +0300

    Edit popup and functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import "./App.css";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Popup from "./components/Popup/Popup";
 import TableInfo from "./components/Table/Table";
+import { data } from "./data";
+import { TableContext } from "./contexts/context";
+import useCrossTabState from "./hooks/useCrossTabState";
+import { cloneDeep, set } from "lodash";
 
 const App = () => {
+  const [tableData, setTableData] = useCrossTabState("tableStore", data);
+
+  const setTableDeepValue = useCallback(
+    (id, value) => {
+      const path = id.replaceAll("-", ".") + ".value";
+      setTableData((prevState) => {
+        const newTable = cloneDeep(prevState);
+        set(newTable, path, value);
+        console.log({ newTable });
+        return newTable;
+      });
+    },
+    [setTableData]
+  );
+
   return (
-    <div className="App">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<TableInfo />} />
-          <Route path="/popup" element={<Popup />} />
-        </Routes>
+    <TableContext.Provider value={[tableData, setTableDeepValue]}>
+      <div className="App">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<TableInfo />} />
+            <Route path="/popup/:id" element={<Popup />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </TableContext.Provider>
   );
 };
 
